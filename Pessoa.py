6c2a1b27a9a7e597db420ff8eb5806d95a268511
Pedro Engel <pedrototiengel@gmail.com>
2024-11-05 21:53:24 -0300

    criacao Classe Pessoa

--- Pessoa.py
+++ Pessoa.py
@@ -1,3 +1,33 @@
+from datetime import date
+
+
 class Pessoa:
-    def __init__(self):
-        self.CPF= 
+
+    def __init__(self, cpf:str, rg:str, nome:str, data:date, cidade:str):
+        self.setCPF(cpf)
+        self.RG = rg
+        self._nome = nome
+        self._data = data
+        self._cidade = cidade
+
+    def setCPF(self, cpf:str):
+        if cpf.isnumeric():
+            self.CPF = cpf
+            return
+        raise TypeError("Valor Inválido para CPF")
+    
+    @property
+    def getCPF(self):
+        return self.CPF
+    
+    @property
+    def getNome(self):
+        return self._nome
+    
+    def __str__(self) -> str:
+        return f"Pessoa {self.getNome} {self.getCPF}"
+    
+
+if __name__ == '__main__':
+    p = Pessoa("054263A6039", "3119606964", "Pedro Engel", date(day=13 , month=1, year=2005), "São Leopoldo" )
+    print(p.getNome)
--- importaCsv.py
+++ importaCsv.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from Pessoa import Pessoa
+from datetime import date
 
-def lerCsv(filePath:str) -> list[tuple]:
+def lerCsv(filePath:str) -> pd.DataFrame:
     try:
         dados = pd.read_csv(filePath, sep = ';', header=None)
     except pd.errors.EmptyDataError:
@@ -9,3 +11,16 @@ def lerCsv(filePath:str) -> list[tuple]:
         return f"Não foi achado o arquivo {filePath}"
     return dados
 
+def criaPessoas(data:list[list]) -> list[Pessoa]:
+    pessoas = []
+    for dado in data:
+        
+        dia, mes, ano = str(dado[3]).split('/')
+        pessoa = Pessoa(cpf=str(dado[0]), rg= dado[1], nome=dado[2], data=date(int(ano), int(mes), int(dia)), cidade = dado[4])  
+        pessoas.append(pessoa)
+    return pessoas
+
+pessoas = criaPessoas(lerCsv("exemplo.csv").values)
+for pessoa in pessoas:
+    print(pessoa)
+# print(lerCsv("exemplo.csv"))
